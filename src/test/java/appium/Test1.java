b8bd9d5905fbe050aef9f2438bdb40403cee0799
Ala123Sobhan <ala123sobhan@gmail.com>
2022-02-13 19:16:04 -0500

    adding basic capabilities for android device

---
+++ src/test/java/appium/Test1.java
@@ -0,0 +1,13 @@
+package appium;
+
+import java.net.MalformedURLException;
+
+public class Test1 extends base{
+	
+	
+	public static void main(String []args) throws MalformedURLException
+	{
+		capabilities();
+	}
+
+}
---
+++ src/test/java/appium/base.java
@@ -0,0 +1,34 @@
+package appium;
+
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class base {
+
+	 public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException
+	{
+		//chrome -url
+		//app .apk, device, 4723
+		//androidDriver 
+		AndroidDriver<AndroidElement> driver;
+
+		DesiredCapabilities cap= new DesiredCapabilities();
+     	File appDir = new File("src/test/java/appium");
+       	File app = new File(appDir, "ApiDemos-debug.apk");
+
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Alaemulator");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");//new step
+		cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        cap.setCapability("newCommandTimeout", "3600");
+		
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        return driver;
+	}
+}
